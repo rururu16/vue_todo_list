09509c6bf206ed2e479544433969b3cd1502704e
rururu16 <ynk618172@gmail.com>
2018-12-15 12:53:01 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    newItem: '',
+    todos: []
+  },
+  methods: {
+    addItem: function(event) {
+      if(this.newItem == '') return;
+      
+      var todo = {
+        item: this.newItem
+      }
+      this.todos.push(todo)
+      this.newItem = ''
+    }
+  }
+})
